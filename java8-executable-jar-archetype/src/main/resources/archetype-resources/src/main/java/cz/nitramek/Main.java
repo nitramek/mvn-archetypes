5b0f8c254d4c63ce41b28b54c6581da58a463fe7
Martin Vaněk <martin.vanek@vrk.cz>
2017-02-18 02:04:28 +0100

    Commit of super simple archetype, which can produce executable jar

---
+++ java8-executable-jar-archetype/src/main/resources/archetype-resources/src/main/java/cz/nitramek/Main.java
@@ -0,0 +1,8 @@
+package cz.nitramek;
+
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+}
---
+++ java8-executable-jar-archetype/src/main/resources/archetype-resources/src/test/java/cz/nitramek/tests/Test.java
@@ -0,0 +1,16 @@
+package cz.nitramek.tests;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+
+    @org.junit.Test
+    public void nashornTest() throws Exception{
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        Integer evaled = (Integer) engine.eval("2+2");
+        assertEquals("2+2", new Integer(4), evaled);
+    }
+}
